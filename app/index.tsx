331c4d20ee52c1a28a35454f6c2568b863bdcdbe
azzhaulia <105841105022@student.unismuh.ac.id>
2025-07-14 14:19:01 +0800

    perbaikan tugas 2.2

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,5 @@
 import React, { useState } from "react";
-import {
-  Image,
-  ScrollView,
-  Text,
-  View,
-  StyleSheet,
-  Pressable,
-} from "react-native";
+import { Image, ScrollView, Text, View, StyleSheet, Pressable} from "react-native";
 
 export default function Index() {
   return (
@@ -18,92 +11,11 @@ export default function Index() {
       }}
     >
       <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-        <View
-          style={{
-            width: 0,
-            height: 0,
-            borderLeftWidth: 200,
-            borderRightWidth: 200,
-            borderBottomWidth: 200,
-            borderStyle: "solid",
-            borderLeftColor: "transparent",
-            borderRightColor: "transparent",
-            borderBottomColor: "red",
-            marginBottom: 10,
-            marginTop: 10,
-          }}
-        ></View>
-
-        <View
-          style={{
-            width: 400,
-            height: 50,
-            backgroundColor: "yellow",
-            marginTop: 10,
-            marginBottom: 10,
-          }}
-        >
-          <Text
-            style={{
-              color: "black",
-              fontWeight: "bold",
-              textAlign: "center",
-              fontSize: 15,
-              lineHeight: 50,
-            }}
-          >
-            Azzah Aulia Syarif
-          </Text>
-        </View>
-
-        <View
-          style={{
-            width: 400,
-            height: 40,
-            backgroundColor: "green",
-            borderRadius: 20,
-            marginTop: 20,
-          }}
-        >
-          <Text
-            style={{
-              color: "black",
-              fontWeight: "bold",
-              textAlign: "center",
-              lineHeight: 40,
-              fontSize: 15,
-            }}
-          >
-            105841105022
-          </Text>
-        </View>
-
-        <View
-          style={{
-            flexDirection: "row",
-          }}
-        >
-          <Image
-            source={{
-              uri: "https://simak.unismuh.ac.id/upload/mahasiswa/105841105022_.jpg?1751871436",
-            }}
-            style={{ width: 200, height: 200, marginTop: 20 }}
-          />
-
-          <Image
-            source={{
-              uri: "https://uploads-us-west-2.insided.com/figma-en/attachment/7105e9c010b3d1f0ea893ed5ca3bd58e6cec090e.gif",
-            }}
-            style={{ width: 200, height: 200, marginTop: 20 }}
-          />
-        </View>
-
         <AzzahGrid />
       </ScrollView>
     </View>
   );
-
-
+}
 function AzzahGrid() {
   const gambarUtama = [
     "https://shopee.co.id/inspirasi-shopee/wp-content/uploads/2022/06/pSpnkIiaRmsaQDu3ipmiA_thumb_469.webp",
@@ -202,4 +114,3 @@ const styles = StyleSheet.create({
     borderRadius: 8,
   },
 });
-}
