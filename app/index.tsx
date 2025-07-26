0683ffc8bcc5091196c9748605d52c90358dc2e0
azzhaulia <105841105022@student.unismuh.ac.id>
2025-07-26 22:28:02 +0800

    tugas4

--- app/index.tsx
+++ app/index.tsx
@@ -1,91 +1,38 @@
-import React, { useState } from "react";
-import { View, Image, StyleSheet, Pressable, ScrollView } from "react-native";
+import { ScrollView, Text } from "react-native";
 
-
-const gambarUtama = [
-  require("../assets/images/Gambar_utama/Gambar_1.jpg"),
-  require("../assets/images/Gambar_utama/Gambar_2.webp"),
-  require("../assets/images/Gambar_utama/Gambar_3.webp"),
-  require("../assets/images/Gambar_utama/Gambar_4.webp"),
-  require("../assets/images/Gambar_utama/Gambar_5.webp"),
-  require("../assets/images/Gambar_utama/Gambar_6.webp"),
-  require("../assets/images/Gambar_utama/Gambar_7.webp"),
-  require("../assets/images/Gambar_utama/Gambar_8.webp"),
-  require("../assets/images/Gambar_utama/Gambar_9.jpg"),
-];
-
-
-const gambarAlternatif = [
-  require("../assets/images/Gambar_alternatif/Gambar_1.jpg"),
-  require("../assets/images/Gambar_alternatif/Gambar_2.jpeg"),
-  require("../assets/images/Gambar_alternatif/Gambar_3.jpg"),
-  require("../assets/images/Gambar_alternatif/Gambar_4.jpeg"),
-  require("../assets/images/Gambar_alternatif/Gambar_5.jpeg"),
-  require("../assets/images/Gambar_alternatif/Gambar_6.jpg"),
-  require("../assets/images/Gambar_alternatif/Gambar_7.webp"),
-  require("../assets/images/Gambar_alternatif/Gambar_8.jpg"),
-  require("../assets/images/Gambar_alternatif/Gambar_9.jpg"),
-];
-
-export default function Azzah() {
-
-  const [imageStates, setImageStates] = useState(
-    Array(gambarUtama.length).fill({ clicked: false, scale: 1.0 })
-  );
-
-
-  const handlePress = (index: number) => {
-    setImageStates((prevStates) =>
-      prevStates.map((state, i) => {
-        if (i === index) {
-          
-          const newScale = Math.min(state.scale + 0.2, 2.0);
-          return { clicked: true, scale: newScale };
-        } else {
-       
-          return { clicked: false, scale: 1.0 };
-        }
-      })
-    );
-  };
-
+export default function Index() {
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <View style={styles.grid}>
-        {gambarUtama.map((gambar, index) => {
-          const { clicked, scale } = imageStates[index];
-          const currentImage = clicked ? gambarAlternatif[index] : gambar;
-
-          return (
-            <Pressable key={index} onPress={() => handlePress(index)}>
-              <Image
-                source={currentImage}
-                style={[styles.image, { transform: [{ scale }] }]}
-              />
-            </Pressable>
-          );
-        })}
-      </View>
+    <ScrollView contentContainerStyle={{ padding: 10 }}>
+      <Text style={{ fontFamily: "B612-Italic", fontSize: 12 }}>
+        FIFIANA {"\n"} 105841104522
+      </Text>
+      <Text style={{ fontFamily: "Fenix-Regular", fontSize: 14 }}>
+        Muh. Akbar Haeruddin {"\n"} 105841104622
+      </Text>
+      <Text style={{ fontFamily: "Gupter-Medium", fontSize: 16 }}>
+        AGUSTIANA {"\n"} 105841104722
+      </Text>
+      <Text style={{ fontFamily: "Marvel-Bold", fontSize: 18 }}>
+        Dia Rahmatillah {"\n"} 105841104822
+      </Text>
+      <Text style={{ fontFamily: "Sniglet-ExtraBold", fontSize: 20 }}>
+        Juliani {"\n"} 105841104922
+      </Text>
+      <Text style={{ fontFamily: "Alkatra-VariableFont", fontSize: 24 }}>
+        Syauqiyah Mujahidah Yahya {"\n"} 105841105122
+      </Text>
+      <Text style={{ fontFamily: "ExpletusSans-VariableFont", fontSize: 26 }}>
+        Mar'atul Azizah {"\n"} 105841105222
+      </Text>
+      <Text style={{ fontFamily: "HostGrotesk-VariableFont", fontSize: 28 }}>
+        Fikrah Lejahtegis {"\n"} 105841105322
+      </Text>
+      <Text style={{ fontFamily: "InclusiveSans-VariableFont", fontSize: 30 }}>
+        Alya Anandha {"\n"} 105841105422
+      </Text>
+      <Text style={{ fontFamily: "PlaywriteAUSA-VariableFont", fontSize: 36 }}>
+        M. Fikri Haikal Ayatullah {"\n"} 105841105522
+      </Text>
     </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    paddingVertical: 20,
-    alignItems: "center",
-  },
-  grid: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    maxWidth: 320,
-    marginTop: 120,
-  },
-  image: {
-    width: 90,
-    height: 90,
-    margin: 5,
-    borderRadius: 10,
-  },
-});
